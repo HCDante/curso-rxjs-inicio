580fbfaef08d151228207cbb0ebd74cf4e74763d
hcdante <hectorhugoandradelinares@gmail.com>
2023-08-15 00:43:50 -0600

    Observable agregado.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,44 @@
+import {Observable, Observer} from 'rxjs';
+
+const observer: Observer<any> = {
+    next: value => console.log('Siguiente [next]: ', value),
+    complete: () => console.log('Completado [obs}'),
+    error: error => console.warn('Error: ', error)
+}
+
+//const obs$ = Observable.create();
+const obs$ = new Observable<string>(pepe => {
+
+        pepe.next('Hola');
+        pepe.next('Mundo');
+        
+        pepe.next('Hola');
+        pepe.next('Mundo2');
+
+        // Forzar un error
+        // const a = undefined;
+        // a.nombre = 'Fernando';
+
+        pepe.complete();
+
+        pepe.next('Hola');
+        pepe.next('Mundo3');
+
+    });
+
+    
 
 
-console.log('Hola Mundo!');
+// obs$.subscribe(resp => {
+//     console.log(resp);
+// });
 
+// obs$.subscribe(console.log);
 
+// obs$.subscribe( 
+//     valor => console.log('Siguiente: ', valor),
+//     error => console.warn('Error: ', error),
+//     () => console.info('Completado')    
+//  );
 
-
-
-
-
+obs$.subscribe(observer);
